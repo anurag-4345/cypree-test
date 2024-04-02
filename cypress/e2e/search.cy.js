3fae6090660e67be8a884fe8868f1715162f0c4a
Anurag <anuragsinha4345@gmail.com>
2024-04-02 08:48:36 +0530

    search files completed
    every element the code is properly work but search suggestion sub section doesn't open when i click on it

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -6,16 +6,25 @@ describe('template spec', () => {
     })
 
     it("find the Search", () => {
-        cy.get('#entry_217822 > .search-wrapper > form > #search > .search-input-group > .search-input > .flex-fill > input')
+        
+            //find and click the categories 
+            cy.get("#entry_217822 > .search-wrapper > form > #search > .search-input-group > .search-input > .dropdown > .btn").click();
+            //click on the sub categorie in any one element
+            cy.get('#entry_217822 > .search-wrapper > form > #search > .search-input-group > .search-input > .dropdown > .dropdown-menu > [data-category_id="25"]').click();
+            
+            // Find the Search input and type the product name
+            cy.get('#entry_217822 > .search-wrapper > form > #search > .search-input-group > .search-input > .flex-fill > input')
             .type('phone')
             .trigger('mouseover')
-
-            // document.querySelector("")
-
-            cy.get("#entry_217822 > .search-wrapper > form > #search > .search-input-group > .search-input > .dropdown > .btn").click();
-            cy.get('#entry_217822 > .search-wrapper > form > #search > .search-input-group > .search-input > .dropdown > .dropdown-menu > [data-category_id="25"]').click();
-
+            
+            // click on the 2nd element in the search suggestion section
+            cy.get(":nth-child(2) > .dropdown-menu > :nth-child(2)")
+            .click()    
+            
+            // click on the search button
             cy.get('[type=submit]').contains('Search').click();
+            // .invoke("val")
+            // .should("eq", "lowestprice")
 
         // cy.get("#search > div.search-input-group.flex-fill > div.dropdown > ul > li:nth-child(1)").children(1).click({force: true,multiple: true })
 
